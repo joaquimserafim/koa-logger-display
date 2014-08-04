aa420cc5e4ae1ec80d44bfacbf1e88ecd6ba5ccd
joaquimserafim <joaquim.serafim@gmail.com>
2014-08-04 22:59:18 +0100

    add colors to info methods

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 var format = require('util').format;
-
+var chalk = require('chalk');
 
 module.exports = display;
 
 function display (logger) {
-  console.log = function () {
-    var args = Array.prototype.slice.call(arguments);
-    process.stdout.write(new Date().toISOString() +
-      '@' +
-      format.apply(format, args)  +
-      '\n');
+
+  var colors = {
+    'ERROR': chalk.red,
+    'WARN': chalk.yellow,
+    'INFO': chalk.green,
+    'LOG': chalk.green
   };
+  
+  ['log', 'info', 'warn', 'error'].forEach(function (k) {
+    console[k] = function () {
+      if ('log' === k)
+        k = 'INFO';
+
+      var args = Array.prototype.slice.call(arguments);
+
+      var log = format.apply(format, args);
+      var method = colors[k.toUpperCase()](k.toUpperCase());
+
+      process.stdout.write(
+        format('%s %s %s\n', 
+          new Date().toISOString(),
+          method, 
+          log));
+    };
+  });
 
   return logger;
 }
